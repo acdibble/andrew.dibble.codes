aadb0d075f89b22be1ef0e6d708db6ec25755c4b
acdibble <acdibble@gmail.com>
2018-04-20 01:32:39 -0500

    Refactor out window.open() for blank anchor tags

--- src/components/Bottom.vue
+++ src/components/Bottom.vue
@@ -1,20 +1,18 @@
 <template>
   <v-footer height="auto">
     <v-layout row wrap justify-center>
-      <v-btn
-        v-for="icon in icons"
-        :id="icon.split('-')[1]"
-        :key="icon"
-        icon
-        @click="handleClick"
+      <a
+        v-for="medium in media"
+        :key="medium.icon"
+        :href="medium.href"
+        target="_blank"
       >
-        <v-icon
-          :id="icon.split('-')[1]"
-          size="24px"
-        >
-          {{ icon }}
-        </v-icon>
-      </v-btn>
+        <v-btn icon>
+          <v-icon size="24px">
+            {{ medium.icon }}
+          </v-icon>
+        </v-btn>
+      </a>
       <v-flex xs12 py-3 text-xs-center>
         &copy; {{ new Date().getFullYear() }} — <strong>Andrew Dibble</strong>
       </v-flex>
@@ -27,26 +25,27 @@
 export default {
   name: 'Bottom',
   data: () => ({
-    icons: [
-      'fab fa-facebook',
-      'fab fa-github-square',
-      'fab fa-linkedin',
-      'fab fa-instagram',
+    media: [
+      {
+        icon: 'fab fa-facebook',
+        href: 'https://www.facebook.com/andrew.dibble',
+      }, {
+        icon: 'fab fa-github-square',
+        href: 'https://github.com/acdibble',
+      }, {
+        icon: 'fab fa-linkedin',
+        href: 'https://www.linkedin.com/in/andrewdibble/',
+      }, {
+        icon: 'fab fa-instagram',
+        href: 'https://www.instagram.com/dynkers/',
+      },
     ],
   }),
-  methods: {
-    handleClick(e) {
-      const { id } = e.target.childNodes[0];
-      if (id === 'facebook') {
-        window.open('https://www.facebook.com/andrew.dibble');
-      } else if (id === 'github') {
-        window.open('https://github.com/acdibble');
-      } else if (id === 'linkedin') {
-        window.open('https://www.linkedin.com/in/andrewdibble/');
-      } else if (id === 'instagram') {
-        window.open('https://www.instagram.com/dynkers/');
-      }
-    },
-  },
 };
 </script>
+
+<style scoped>
+a {
+    text-decoration: none !important;
+}
+</style>
