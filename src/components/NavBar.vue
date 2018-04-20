1f845b11e55d398baa19e44d1a0bcfdd1f87643a
acdibble <acdibble@gmail.com>
2018-04-20 01:03:40 -0500

    Add home button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,8 +4,9 @@
     <v-toolbar-title>Welcome</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat @click="handleClick">Home</v-btn>
       <v-btn flat>Portfolio</v-btn>
-      <v-btn @click="handleClick">Contact</v-btn>
+      <v-btn flat @click="handleClick">Contact</v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
@@ -17,6 +18,8 @@ export default {
     handleClick({ target }) {
       if (target.innerText === 'CONTACT') {
         this.$router.push('/contact');
+      } else if (target.innerText === 'HOME') {
+        this.$router.push('/');
       }
     },
   },
