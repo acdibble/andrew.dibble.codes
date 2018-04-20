7cf71830141ce046e0fad2038edecedd8cf54f37
acdibble <acdibble@gmail.com>
2018-04-20 00:50:42 -0500

    Add contact view to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+
 import Main from '@/components/Main';
+import Contact from '@/components/Contact';
 
 Vue.use(Router);
 
@@ -10,6 +12,10 @@ export default new Router({
       path: '/',
       name: 'Main',
       component: Main,
+    }, {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact,
     },
   ],
 });
