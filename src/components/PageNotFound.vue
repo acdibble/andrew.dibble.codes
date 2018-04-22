a9b16c2ec6a7dd09efb79a45f539cdf525e28309
acdibble <acdibble@gmail.com>
2018-04-22 01:45:18 -0500

    Add 404 page

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>404. Not found.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFound',
+};
+</script>
+
+<style scoped>
+h1 {
+  width:800px;
+  height: 200px; line-height: 200px;
+  position: absolute;
+  top: 50%; margin-top:-100px;
+  left: 50%; margin-left:-400px;
+  font-size: 100px;
+}
+</style>
