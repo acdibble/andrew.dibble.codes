2f67674498c914205d52382737ee7d980134525e
acdibble <acdibble@gmail.com>
2018-04-27 02:43:19 -0500

    Refactor line spacing in html of navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,21 +4,12 @@
     <v-toolbar-title>Welcome</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn
-        v-for="route in routes"
-        :key="route"
-        @click="navigate(route)"
-        flat
-      >{{ route }}</v-btn>
+      <v-btn v-for="route in routes" :key="route" @click="navigate(route)" flat >{{ route }}</v-btn>
     </v-toolbar-items>
     <v-menu class="hidden-md-and-up">
       <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
       <v-list>
-        <v-list-tile
-          v-for="route in routes"
-          :key="route"
-          @click="navigate(route)"
-        >
+        <v-list-tile v-for="route in routes" :key="route" @click="navigate(route)" >
           <v-list-tile-title>{{ route }}</v-list-tile-title>
         </v-list-tile>
       </v-list>
