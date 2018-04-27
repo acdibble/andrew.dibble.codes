366478c74005389be4e8f4c78f68ab329b1ddc57
acdibble <acdibble@gmail.com>
2018-04-26 23:58:42 -0500

    Add portfolio placeholder

---
+++ src/components/Portfolio.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Coming soon.</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Portfolio',
+};
+</script>
+
+<style scoped>
+h1 {
+  width:800px;
+  height: 200px; line-height: 200px;
+  position: absolute;
+  top: 50%; margin-top:-100px;
+  left: 50%; margin-left:-400px;
+  font-size: 100px;
+}
+</style>
