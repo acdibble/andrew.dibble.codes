3c2fe305008af5d108afb8706a0dfc8a963f0206
acdibble <acdibble@gmail.com>
2018-04-27 01:12:34 -0500

    Add card with portrait and email to contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,12 +1,39 @@
 <template>
-  <div>
-    <h1>Coming soon.</h1>
+  <div id="card">
+    <v-layout>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-media :src="require('@/assets/img/portrait.jpg')" height="300px">
+          </v-card-media>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Trying to reach me?</h3>
+              <!-- eslint-disable-next-line -->
+              <br />
+              <div>Please feel free to contact me via the social media in the footer or at
+                <a :href="'mailto:' + email">{{ email }}</a>.</div>
+            </div>
+          </v-card-title>
+          <!-- <v-card-actions>
+            <v-tooltip top>
+              <v-btn flat color="primary" slot="activator" :href="'mailto:' + email">Email</v-btn>
+              <span>{{ email }}</span>
+            </v-tooltip>
+          </v-card-actions> -->
+        </v-card>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Contact',
+  data() {
+    return {
+      email: 'me@andrewdibble.com',
+    };
+  },
 };
 </script>
 
@@ -18,5 +45,9 @@ h1 {
   top: 50%; margin-top:-100px;
   left: 50%; margin-left:-400px;
   font-size: 100px;
+}
+
+#card {
+  margin: 50px;
 }
 </style>
