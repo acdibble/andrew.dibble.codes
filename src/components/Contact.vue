fc07da8c1086643e3995c1a2560a99078d0315c5
acdibble <acdibble@gmail.com>
2018-04-29 21:40:36 -0500

    Change concatentation to template literal in contact card

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -9,7 +9,7 @@
               h3(class="headline mb-0") Trying to reach me?
               br
               div Please feel free to contact me via the social media in the footer or at
-                |  #[a(:href="'mailto:' + email") {{ email }}]
+                |  #[a(:href="`mailto:${email}`") {{ email }}]
 </template>
 
 <script>
