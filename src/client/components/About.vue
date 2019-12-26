c02d56999bc44d99c19188bdb2e5de7acd4a9b34
Andrew Dibble <acdibble@gmail.com>
2019-12-26 07:18:47 -0600

    Update bio

--- src/client/components/About.vue
+++ src/client/components/About.vue
@@ -6,17 +6,19 @@
       p My name is Andrew Dibble, and I am a software engineer.
       p In the past, I've worked in German parliament, in a law office, as a heavy equipment
         |  operator, and even as a Sandwich Artist at Subway. I hold a Bachelor of Science in
-        |  German and International Business from Canisius College in Buffalo. But in 2017, I
-        |  decided to finally take the plunge and jump head first into my new career.
-      p I live in Austin, TX, but I originally am from the Buffalo, NY area. I moved here to pursue
-        |  a career in software engineering, attending an advanced software engineering immersive
-        |  program before ultimately landing a job as a software engineer at Malauzai Software,
-        |  Inc., an Austin-based start up which was recently acquired by Finastra.
-      p At Malauzai, I work behind the scenes as a back-end engineer, ensuring your money gets to
-        |  where you want it to be. Otherwise, you'll find me playing games, avoiding the
-        |  scorching summer, and attempting to keep up on new techs and learning some old ones.
-        |  Feel free to reach out to me via the contact page or connect with me on the social
-        |  media in the footer.
+        |  German and International Business from Canisius College in Buffalo. Currently, however,
+        |  I am a software engineer who specializes in Node.JS based out of Berlin, Germany.
+      p Although I have worked most extensively with the Node.JS runtime, I prefer to think of
+        |  myself as language agnostic. I like to research and find the best tools for the task at
+        |  hand as well as dabble in different technologies and languages and see what they have to
+        |  offer
+      p As such, I've worked with web frameworks like Ruby on Rails and Elixir and Phoenix,
+        |  databases like PostgreSQL (and other flavors of SQL) and MongoDB (which stores my
+        |  projects in the projects section), various cloud service providers to varying degrees,
+        |  various front-end JS libraries, and, to a more limited extent, some native frameworks
+        |  and languages.
+      p Outside of tech, I like to read comedic fiction and poetry, attempt to finally learn an
+        |  instrument, ride motorcycles, and play the occasional video game or two.
 </template>
 
 <script>
@@ -38,4 +40,3 @@ p {
   margin-right: 10vw;
 }
 </style>
-
