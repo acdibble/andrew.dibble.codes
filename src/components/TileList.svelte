7d7734abd7abe21720f3e1b50145eed88cc80886
Andrew Dibble <acdibble@gmail.com>
2020-04-26 18:19:06 +0200

    Add more info to tilelist

--- src/components/TileList.svelte
+++ src/components/TileList.svelte
@@ -9,14 +9,27 @@
   h1 {
     margin-top: 0;
   }
+
+  @media (min-width: 768px) {
+    .mobile-warning {
+      display: none;
+    }
+  }
 </style>
 
 <h1>What I've learned:</h1>
+<p>
+  A collection of concise write-ups on small things I learn day to day across a
+  variety of languages and technologies.
+</p>
+<small class="mobile-warning">
+  The code snippets may look terrible on mobile.
+</small>
 {#await request}
   <p>Fetching TILs...</p>
 {:then structure}
   {#each Object.keys(structure) as category}
-    <section class="tile-category">
+    <section>
       <h2>{category}</h2>
       {#each structure[category] as title}
         <Tile {title} {category} />
