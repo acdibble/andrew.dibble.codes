47acdd92be1864336b8e26cf3e1f012bc5bbed7b
Andrew Dibble <andrew@dibble.codes>
2022-06-13 23:19:15 +0200

    add link icon

--- pages/tils/index.tsx
+++ pages/tils/index.tsx
@@ -23,7 +23,21 @@ export default function Tils({ tils }: TilsProps): JSX.Element {
             <Card.Body>
               <ul>
                 {tils[category].map((name) => (
-                  <li key={name} className="prose ">
+                  <li key={name} className="prose flex items-center space-x-1">
+                    <svg
+                      className="h-3 w-3"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+                      />
+                    </svg>
                     <Link href={formatPath(category, name)}>
                       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                       <a
