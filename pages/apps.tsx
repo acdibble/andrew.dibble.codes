dcbf7d6c14954fd3b755d9dc8a8a757ba170edfa
Andrew Dibble <andrew@dibble.codes>
2022-06-13 21:04:15 +0200

    add external link icon

--- pages/apps.tsx
+++ pages/apps.tsx
@@ -27,8 +27,27 @@ export default function Apps() {
           <div className="flex w-full flex-col" key={title}>
             <Card image={<Image {...image} />}>
               <Card.Title>
-                <a href={url} target="_blank" rel="noreferrer">
-                  {title}
+                <a
+                  href={url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flex items-center space-x-1"
+                >
+                  <span>{title}</span>
+                  <svg
+                    className="h-5 w-5"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                    />
+                  </svg>
                 </a>
               </Card.Title>
               <Card.Body>{body}</Card.Body>
